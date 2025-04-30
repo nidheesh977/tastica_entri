7f57a4537ffcd605d8ade564ee76d51d09949109
arjun-saseendran <arjunsaseendran@icloud.com>
2025-04-30 11:55:35 +0530

    update ProtectedRouteAdmin

--- client/src/router/ProtectedRouteAdmin.jsx
+++ client/src/router/ProtectedRouteAdmin.jsx
@@ -1,5 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export const ProtectedRouteAdmin = () => {
+  const isAdmin = useSelector((state) => state?.auth?.adminData);
+  const navigate = useNavigate();
+
+  if (!isAdmin) {
+    navigate("/shop/admin/login");
+    return;
+  }
   return <Outlet />;
 };
