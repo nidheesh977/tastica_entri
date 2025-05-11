755ba666bc8e65360252d54505a1176071f71ff9
arjun-saseendran <arjunsaseendran@icloud.com>
2025-05-11 12:51:47 +0530

    update store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import sidebarReducer from "./features/sidebarSlice";
-import categoryReducer from "./features/categorySlice";
-import productReducer from "./features/productSlice";
-import customerReducer from './features/customerSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     sidebar: sidebarReducer,
-    categories: categoryReducer,
-    products: productReducer,
-    customers: customerReducer
   },
 });
