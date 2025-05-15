31005f0e40f29121c31a3a589f88e0a0f7d3d331
arjun-saseendran <arjunsaseendran@icloud.com>
2025-05-15 19:27:37 +0530

    add PayDialogueBox

---
+++ client/src/components/shared/PayDialogueBox/PayDialogueBox.jsx
@@ -0,0 +1,58 @@
+import { FaDollarSign, FaTimes } from "react-icons/fa";
+import { FaMoneyCheckAlt } from "react-icons/fa";
+
+export const PayDialogueBox = ({
+  message,
+  onPayNow,
+  onOnlinePay,
+  onCancel,
+  invoice,
+}) => {
+  return (
+    <div
+      className={`fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+                  p-10 rounded-lg shadow-lg z-50 min-w-[500px] h-96 text-center bg-tertiary text-black `}
+    >
+      <ul className="font-semibold flex flex-col gap-1 ">
+        <li className="border  p-2 shadow">Subtotal: MVR{invoice?.subTotal}</li>
+        <li className="border  p-2 shadow">Tax: 0</li>
+        <li className="border p-2 shadow">Total: MVR{invoice?.totalAmount}</li>
+        <li className="border p-2 shadow">{message}</li>
+        <ul className="flex items-center gap-4">
+          <li>
+            <input
+              type="text"
+              className="py-2 text-base outline-primary text-center border placeholder:p-3 shadow"
+              placeholder="Received Amount"
+            />
+          </li>
+          <li className="border flex items-center justify-between p-2 w-full shadow">Balance: MVR50</li>
+        </ul>
+      </ul>
+
+      <div className="flex justify-center gap-4 mb-4 mt-5">
+        <button
+          onClick={onPayNow}
+          className="flex items-center justify-center  gap-2 px-6 py-3 bg-primary text-sm font-semibold rounded text-white hover:bg-opacity-90"
+        >
+          <FaDollarSign />
+          Cash
+        </button>
+        <button
+          onClick={onOnlinePay}
+          className=" flex items-center justify-center gap-2 px-6 py-3 bg-primary text-sm font-semibold rounded text-white hover:bg-opacity-90"
+        >
+          <FaMoneyCheckAlt />
+          Pay
+        </button>
+        <button
+          onClick={onCancel}
+          className="px-6 py-3 flex items-center justify-center gap-2 bg-secondary text-sm font-semibold rounded text-white hover:bg-opacity-90"
+        >
+          <FaTimes />
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+};
--- client/src/components/shared/ShoppingCart/ShoppingCart.jsx
+++ client/src/components/shared/ShoppingCart/ShoppingCart.jsx
@@ -6,6 +6,7 @@ import { useInvoices } from "../../../hooks/useInvoices";
 import { MdShoppingCart } from "react-icons/md";
 import { useSelector } from "react-redux";
 import { AlertBox } from "../AlertBox/AlertBox";
+import { PayDialogueBox } from "../PayDialogueBox/PayDialogueBox";
 
 export const ShoppingCart = () => {
   const { customers, addCustomer } = useCustomers();
@@ -20,8 +21,12 @@ export const ShoppingCart = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [mobile, setMobile] = useState("");
   const [customerId, setCustomerId] = useState("");
-  const [quantities, setQuantities] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [selectedProductId, setSelectedProductId] = useState(null);
   const [alertMessage, setAlertMessage] = useState(null);
+  const [receivedAmount, setReceivedAmount] = useState("");
+  const [payMode, setPayMode] = useState(null);
+  const [showPayDialog, setShowPayDialog] = useState(false);
 
   const findCustomer = () => {
     const isTenDigits = /^\d{10}$/.test(searchQuery);
@@ -45,6 +50,20 @@ export const ShoppingCart = () => {
       setName("");
       setIsNewCustomer(true);
     }
+  };
+
+  const handlePayNow = () => {
+    setPayMode("cash");
+    setShowPayDialog(false);
+  };
+
+  const handleOnlinePay = () => {
+    setPayMode("online");
+    setShowPayDialog(false);
+  };
+
+  const handleCancel = () => {
+    setShowPayDialog(false);
   };
 
   useEffect(() => {
@@ -129,7 +148,7 @@ export const ShoppingCart = () => {
         )}
       </div>
 
-      <ul className="flex flex-col mt-4 h-[387px] overflow-y-auto w-full">
+      <ul className="flex flex-col mt-4 h-[382px] overflow-y-auto w-full">
         {products?.map((product, index) => (
           <li
             key={product?._id}
@@ -152,19 +171,26 @@ export const ShoppingCart = () => {
             </span>
             <div className="flex items-center gap-2 col-span-12 xl:col-span-4 my-2 xl:my-0 mx-auto xl:mx-0">
               <input
-                value={quantities[product?._id] || 1}
-                className="w-14 h-8 text-center rounded bg-tertiary "
+                value={selectedProductId === product._id ? quantity : 1}
                 type="number"
+                className="w-14 h-8 text-center rounded bg-tertiary"
+                onFocus={() => {
+                  setSelectedProductId(product._id);
+                  setQuantity(1);
+                }}
                 onChange={(e) => {
-                  const newQty = Number(e.target.value);
-                  setQuantities((prev) => ({
-                    ...prev,
-                    [product._id]: newQty,
-                  }));
-                  addProductToInvoice({
-                    productId: product._id,
-                    quantity: newQty,
-                  });
+                  const value = e.target.value;
+                  const newQty = value === "" ? "" : Number(value);
+
+                  if (selectedProductId === product._id) {
+                    setQuantity(newQty);
+                    if (newQty !== "") {
+                      addProductToInvoice({
+                        productId: product._id,
+                        quantity: newQty,
+                      });
+                    }
+                  }
                 }}
               />
 
@@ -183,7 +209,7 @@ export const ShoppingCart = () => {
       <div className="mt-2 w-full font-bold">
         <div className="flex justify-between items-center border px-2 py-2">
           <div>Subtotal</div>
-          <div>MVR{invoice?.subTotal}</div>
+          <div>MVR{Math.round(invoice?.subTotal) || 0}</div>
         </div>
         <div className="flex justify-between items-center border px-2 py-2">
           <div>Tax</div>
@@ -191,18 +217,34 @@ export const ShoppingCart = () => {
         </div>
         <div className="flex justify-between items-center font-semibold border px-2 py-2">
           <div>Total</div>
-          <div>MVR{invoice?.totalAmount}</div>
+          <div>MVR{Math.round(invoice?.totalAmount) || 0}</div>
         </div>
       </div>
 
       <div className="flex gap-2 mt-2 justify-between">
-        <button className="flex items-center justify-center gap-2 px-6 py-3 w-1/2 bg-[#BF3131] hover:bg-opacity-90 text-white rounded-lg">
+        <button className="flex items-center justify-center gap-2 px-6 py-3 w-1/2 bg-secondary hover:bg-opacity-90 text-white rounded-lg">
           <FaSave /> Save
         </button>
-        <button className="flex items-center justify-center gap-2 px-6 py-3 w-1/2 bg-[#155E95] hover:bg-opacity-90 text-white rounded-lg">
+
+        <button
+          className="flex items-center justify-center gap-2 px-6 py-3 w-1/2 bg-primary hover:bg-opacity-90 text-white rounded-lg"
+          onClick={() => setShowPayDialog(true)}
+        >
           <FaMoneyCheckAlt /> Pay
         </button>
       </div>
+
+      {showPayDialog && (
+        <PayDialogueBox
+          message={`Total payable amount: MVR${
+            Math.round(invoice?.totalAmount) || 0
+          }`}
+          onPayNow={handlePayNow}
+          onOnlinePay={handleOnlinePay}
+          onCancel={handleCancel}
+          invoice={invoice}
+        />
+      )}
     </div>
   );
 };
