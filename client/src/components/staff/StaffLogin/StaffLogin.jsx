e1f09ad8ebb62915c3c4c5633b0740604cb0e835
arjun-saseendran <arjunsaseendran@icloud.com>
2025-05-28 13:35:07 +0530

    fix side bar

--- client/src/components/staff/StaffLogin/StaffLogin.jsx
+++ client/src/components/staff/StaffLogin/StaffLogin.jsx
@@ -35,59 +35,57 @@ export const StaffLogin = () => {
   };
 
   return (
-    
-      <form
-        onSubmit={(e) => e.preventDefault()}
-        className="my-20
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="my-20
           w-full md:w-[500px] py-10 md:pb-20 px-4 md:px-10 bg-tertiary text-primary shadow-2xl border rounded-lg"
-      >
-        <h1 className="text-3xl py-4 font-thin text-center text-primary ">
-          Staff Login
-        </h1>
+    >
+      <h1 className="text-3xl py-4 font-thin text-center text-primary ">
+        Staff Login
+      </h1>
 
+      <input
+        type="text"
+        value={phoneNumber}
+        placeholder="Mobile"
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        className="p-4 my-1 w-full bg-white shadow-2xl outline-primary"
+      />
+      <div className="relative w-full my-1">
         <input
-          type="text"
-          value={phoneNumber}
-          placeholder="Mobile"
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          className="p-4 my-1 w-full bg-white shadow-2xl outline-primary"
+          type={passwordShow ? "text" : "password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          className="p-4 pr-10 w-full bg-white shadow-2xl outline-primary "
         />
-        <div className="relative w-full my-1">
-          <input
-            type={passwordShow ? "text" : "password"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="p-4 pr-10 w-full bg-white shadow-2xl outline-primary "
-          />
-          <button
-            type="button"
-            className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400"
-            onClick={() => setPasswordShow((prev) => !prev)}
-          >
-            {passwordShow ? (
-              <FaEye
-                className={passwordShow ? "text-primary" : "text-gray-400"}
-              />
-            ) : (
-              <FaEyeSlash />
-            )}
-          </button>
-        </div>
-
         <button
-          className="p-4  bg-primary mt-2 hover:opacity-90 w-full text-white rounded-lg"
-          onClick={() => {
-            handleLogin();
-            setPhoneNumber("");
-            setPassword("");
-          }}
+          type="button"
+          className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400"
+          onClick={() => setPasswordShow((prev) => !prev)}
         >
-          <span className="flex items-center justify-center gap-2 font-bold">
-            Login In <FaSignInAlt />
-          </span>
+          {passwordShow ? (
+            <FaEye
+              className={passwordShow ? "text-primary" : "text-gray-400"}
+            />
+          ) : (
+            <FaEyeSlash />
+          )}
         </button>
-      </form>
-   
+      </div>
+
+      <button
+        className="p-4  bg-primary mt-2 hover:opacity-90 w-full text-white rounded-lg"
+        onClick={() => {
+          handleLogin();
+          setPhoneNumber("");
+          setPassword("");
+        }}
+      >
+        <span className="flex items-center justify-center gap-2 font-bold">
+          Login In <FaSignInAlt />
+        </span>
+      </button>
+    </form>
   );
 };
