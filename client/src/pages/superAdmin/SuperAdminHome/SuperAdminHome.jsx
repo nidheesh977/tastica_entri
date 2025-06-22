f075bd9894dc5832c840a8bf879a518d1f21faed
arjun-saseendran <arjunsaseendran@icloud.com>
2025-06-22 21:57:51 +0530

    update admin home

--- client/src/pages/superAdmin/SuperAdminHome/SuperAdminHome.jsx
+++ client/src/pages/superAdmin/SuperAdminHome/SuperAdminHome.jsx
@@ -4,34 +4,84 @@ import { MonthlySales } from "../../../components/admin/MonthlySales/MonthlySale
 import { SalesPerformance } from "../../../components/admin/SalesPerformance/SalesPerformance";
 import { YearlySales } from "../../../components/admin/YearlySales/YearlySales";
 import { SalesChart } from "../../../components/admin/SalesChart/SalesChart";
+import { useState } from "react";
+import { useDashboard } from "../../../hooks/useDashboard";
 
 export const SuperAdminHome = () => {
+  const [selectedMethodMonth, setSelectedMethodMonth] = useState("all");
+  const [selectedMethodWeek, setSelectedMethodWeek] = useState("all");
+  const [selectedMethodYear, setSelectedMethodYear] = useState("all");
+  const [date, setDate] = useState("");
+  const [year, setYear] = useState(0);
+  const [customYear, setCustomYear] = useState(0);
+  const [month, setMonth] = useState("");
+  const [customMonth, setCustomMonth] = useState("");
+  const [day, setDay] = useState("");
+  const {
+    categoriesSalesDataAdmin,
+    monthSalesBarChartDataAdmin,
+    weeklySalesBarChartDataAdmin,
+    yearlySalesBarChartDataAdmin,
+    dateSalesAdmin,
+    monthSalesAdmin,
+    yearSalesAdmin,
+    monthSalesLineChartDataAdmin,
+  } = useDashboard({
+    selectedMethodMonth,
+    selectedMethodWeek,
+    selectedMethodYear,
+    year,
+    month,
+    day,
+    customMonth,
+    customYear,
+  });
+
   return (
     <>
-      <div className="lg:h-[650px] p-1">
-        <section className="grid grid-cols-12 gap-2">
-          <div className="col-span-12 lg:col-span-4">
-            <CategorySales />
-          </div>
-          <div className="col-span-12 lg:col-span-5 xl:col-span-4">
-            <SalesPerformance />
-          </div>
-          <div className="col-span-12 lg:col-span-3 xl:col-span-4">
-            <SalesChart />
-          </div>
-        </section>
-        <section className="grid grid-cols-12 gap-2 mt-2">
-          <div className="col-span-12 lg:col-span-4">
-            <WeeklySales />
-          </div>
-          <div className="col-span-12 lg:col-span-4">
-            <MonthlySales />
-          </div>
-          <div className="col-span-12 lg:col-span-4">
-            <YearlySales />
-          </div>
-        </section>
+      <div className="p-2 gap-2 grid grid-cols-12">
+        <div className="col-span-12 md:col-span-6  lg:col-span-4">
+          <CategorySales invoices={categoriesSalesDataAdmin} />
+        </div>
+        <div className="col-span-12 md:col-span-6  lg:col-span-4">
+          <SalesPerformance
+            date={date}
+            customMonth={customMonth}
+            customYear={customYear}
+            setYear={setYear}
+            setDate={setDate}
+            setMonth={setMonth}
+            setCustomMonth={setCustomMonth}
+            setCustomYear={setCustomYear}
+            setDay={setDay}
+            dateSales={dateSalesAdmin}
+            yearSales={yearSalesAdmin}
+            monthSales={monthSalesAdmin}
+          />
+        </div>
+        <div className="col-span-12 md:col-span-6  lg:col-span-4 ">
+          <SalesChart invoices={monthSalesLineChartDataAdmin} />
+        </div>
+
+        <div className="col-span-12 md:col-span-6  lg:col-span-4">
+          <WeeklySales
+            invoices={weeklySalesBarChartDataAdmin}
+            method={setSelectedMethodWeek}
+          />
+        </div>
+        <div className="col-span-12 md:col-span-6  lg:col-span-4">
+          <MonthlySales
+            invoices={monthSalesBarChartDataAdmin}
+            method={setSelectedMethodMonth}
+          />
+        </div>
+        <div className="col-span-12 md:col-span-6  lg:col-span-4">
+          <YearlySales
+            invoices={yearlySalesBarChartDataAdmin}
+            method={setSelectedMethodYear}
+          />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
