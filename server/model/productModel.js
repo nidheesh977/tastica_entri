7662f68fb6327325aa0bf40cc2c8cef310cdf4e1
Akshai Dev <141032635+Akshai-muraleedharan@users.noreply.github.com>
2025-06-26 11:36:44 +0530

    Update productModel.js

--- server/model/productModel.js
+++ server/model/productModel.js
@@ -65,7 +65,11 @@ const productSchema = new mongoose.Schema({
     isActive:{
         type:Boolean,
         default:false
-    }
+    }, 
+   loyalityRate:{
+      type:Number,
+      default:0
+   } 
 
 },{timestamps:true});
 
