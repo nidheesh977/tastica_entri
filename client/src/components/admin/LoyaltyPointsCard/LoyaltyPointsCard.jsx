dec4664acc10e6bb94f7b98d0afb11d904a7807f
arjun-saseendran <arjunsaseendran@icloud.com>
2025-06-26 16:37:56 +0530

    add loyalty point

--- client/src/components/admin/LoyaltyPointsCard/LoyaltyPointsCard.jsx
+++ client/src/components/admin/LoyaltyPointsCard/LoyaltyPointsCard.jsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
-import { FiGift, FiRefreshCcw } from "react-icons/fi";
-import { MdDelete } from "react-icons/md";
+import { FiGift } from "react-icons/fi";
 import { useLoyaltyPoints } from "../../../hooks/useLoayltyPoints";
 
 export const LoyaltyPointsCard = () => {
-  const [loyaltyRate, setLoyaltyRate] = useState(null);
-  const [updatedLoyaltyPoints, setUpdatedLoyaltyPoints] = useState(null);
-  const {
-    loyaltyPoints,
-    addLoyaltyPoints,
-    updateLoyaltyPoints,
-    deleteLoyaltyPoints,
-  } = useLoyaltyPoints();
+  const [loyaltyRate, setLoyaltyRate] = useState("");
+
+  const { addLoyaltyPoints } = useLoyaltyPoints();
 
   return (
     <>
@@ -22,75 +16,30 @@ export const LoyaltyPointsCard = () => {
                         my-10  md:w-[500px] md:h-72 p-4 md:p-10   md:px-10 bg-tertiary text-primary shadow-2xl rounded-lg"
         >
           <h1 className="md:text-3xl mb-6 font-thin text-center text-primary ">
-            Manage Loyalty Points
+            Add Loyalty Points To Product
           </h1>
 
-          {loyaltyPoints?.loyalityRate && (
-            <div className=" flex items-center gap-2">
-              <input
-                type="text"
-                value={loyaltyPoints?.loyalityRate}
-                readOnly
-                className="p-4 my-1 w-full bg-white shadow-2xl outline-none text-primary"
-              />
-              <button
-                onClick={() => deleteLoyaltyPoints({ id: loyaltyPoints?._id })}
-                className="p-4 w-26   bg-red-600 hover:opacity-90  text-white rounded"
-              >
-                <span className="flex items-center justify-center gap-2 font-semibold">
-                  Delete <MdDelete />
-                </span>
-              </button>
-            </div>
-          )}
-          {!loyaltyPoints?.loyalityRate && (
-            <div className=" flex items-center gap-2">
-              <input
-                type="text"
-                value={loyaltyRate}
-                onChange={(e) => setLoyaltyRate(e.target.value)}
-                placeholder="Loyalty Rate "
-                className="p-4 my-1 w-full bg-white shadow-2xl outline-primary"
-              />
-              <button
-                onClick={() => {
-                  addLoyaltyPoints({ loyalityRate: loyaltyRate });
-                  setLoyaltyRate("");
-                }}
-                className="p-4 w-26   bg-primary hover:opacity-90  text-white rounded"
-              >
-                <span className="flex items-center justify-center gap-1 font-semibold">
-                  Submit
-                  <FiGift />
-                </span>
-              </button>
-            </div>
-          )}
-          {loyaltyPoints?.loyalityRate && (
-            <div className="flex items-center gap-2">
-              <input
-                type="text"
-                value={updatedLoyaltyPoints}
-                onChange={(e) => setUpdatedLoyaltyPoints(e.target.value)}
-                placeholder="Update Loyalty Rate "
-                className="p-4 my-1 w-full bg-white shadow-2xl outline-primary"
-              />
-              <button
-                onClick={() => {
-                  updateLoyaltyPoints({
-                    loyalityRate: updatedLoyaltyPoints,
-                    id: loyaltyPoints?._id,
-                  });
-                  setUpdatedLoyaltyPoints("");
-                }}
-                className="p-4 w-26   bg-secondary hover:opacity-90  text-white rounded"
-              >
-                <span className="flex items-center justify-center gap-1 font-semibold">
-                  Update <FiRefreshCcw />
-                </span>
-              </button>
-            </div>
-          )}
+          <div className=" flex items-center gap-2">
+            <input
+              type="text"
+              value={loyaltyRate}
+              onChange={(e) => setLoyaltyRate(e.target.value)}
+              placeholder="Loyalty Rate "
+              className="p-4 my-1 w-full bg-white shadow-2xl outline-primary"
+            />
+            <button
+              onClick={() => {
+                addLoyaltyPoints(loyaltyRate );
+                setLoyaltyRate("");
+              }}
+              className="p-4 w-26   bg-primary hover:opacity-90  text-white rounded"
+            >
+              <span className="flex items-center justify-center gap-1 font-semibold">
+                Submit
+                <FiGift />
+              </span>
+            </button>
+          </div>
         </form>
       </div>
     </>
--- client/src/hooks/useLoayltyPoints.js
+++ client/src/hooks/useLoayltyPoints.js
@@ -1,91 +1,28 @@
 import toast from "react-hot-toast";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { axiosInstance } from "../config/axiosInstance";
 
 export const useLoyaltyPoints = () => {
-  const queryClient = useQueryClient();
-  const { data } = useQuery({
-    queryKey: ["loyaltyPoints"],
-    queryFn: async () => {
-      const response = await axiosInstance({
-        method: "GET",
-        url: "/loyality",
-        withCredentials: true,
-      });
-      return response?.data?.data;
-    },
-  });
-
   const { mutate: addLoyaltyPoints } = useMutation({
-    mutationFn: async ({ loyalityRate }) => {
-      const data = {
-        loyalityRate,
-      };
+    mutationFn: async (loyaltyRate) => {
       await axiosInstance({
-        method: "POST",
-        url: "/loyality",
+        method: "PUT",
+        url: "/loyalty/products",
         withCredentials: true,
-        data,
+        loyaltyRate,
       });
     },
     onSuccess: () => {
-      toast.success("Loyalty point added successfully!");
-      queryClient.invalidateQueries(["loyaltyPoints"]);
+      toast.success("Loyalty point set successfully to products.");
     },
     onError: (error) => {
       toast.error(
-        error?.response?.data?.message || "Failed to add loyalty points."
-      );
-    },
-  });
-  const { mutate: deleteLoyaltyPoints } = useMutation({
-    mutationFn: async ({id}) => {
-      await axiosInstance({
-        method: "DELETE",
-        url: `/loyality/${id}`,
-        withCredentials: true,
-      });
-    },
-    onSuccess: () => {
-      toast.success("Loyalty deleted successfully!");
-      queryClient.invalidateQueries(["loyaltyPoints"]);
-    },
-    onError: (error) => {
-      toast.error(
-        error?.response?.data?.message || "Failed to delete loyalty point."
-      );
-    },
-  });
-
-  const { mutate: updateLoyaltyPoints } = useMutation({
-    mutationFn: async ({ loyalityRate, id }) => {
-      const data = {
-        loyalityRate,
-      };
-
-      await axiosInstance({
-        method: "PATCH",
-        url: `/loyality/${id}`,
-        withCredentials: true,
-        data,
-      });
-    },
-    onSuccess: () => {
-      toast.success("Loyalty points updated successfully!");
-      queryClient.invalidateQueries(["loyaltyPoints"]);
-    },
-
-    onError: (error) => {
-      toast.error(
-        error?.response?.data?.message || "Failed to update loyalty points."
+        error?.response?.data?.message || "Failed to set loyalty points!"
       );
     },
   });
 
   return {
-    loyaltyPoints: data,
     addLoyaltyPoints,
-    updateLoyaltyPoints,
-    deleteLoyaltyPoints,
   };
 };
