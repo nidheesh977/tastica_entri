6628c5ab9840988d160e6889fa3047bfe20b603c
Akshai-muraleedharan <akshaimuraleedharan4@gmail.com>
2025-07-14 11:42:49 +0530

    code update in wallet model and controller

--- server/model/walletModel.js
+++ server/model/walletModel.js
@@ -1,21 +1,26 @@
 import mongoose from "mongoose";
 
 const walletSchema = new mongoose.Schema({
-    customerId:{ type:mongoose.Schema.Types.ObjectId,ref:"Customer",unique:true, required:true},
-    balance:{type:Number,default:0}
-},{timestamps:true})
+    customerId: { type: mongoose.Schema.Types.ObjectId, ref: "Customer", unique: true, required: true },
+    shopId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Shop",
+    },
+    balance: { type: Number, default: 0 }
+}, { timestamps: true })
 
 
 const walletTransactionSchema = new mongoose.Schema({
-    customerId:{ type:mongoose.Schema.Types.ObjectId,ref:"Customer",required:true},
-    staffId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
-    amount:{type:Number,required:true},
-    type:{type:String,enum:["credit","debit"],required:true},
-    balanceAfterTransaction:{type:Number,default:0},
-    date:{type:Date,default:Date.now()}
-},{timestamps:true})
+    customerId: { type: mongoose.Schema.Types.ObjectId, ref: "Customer", required: true },
+    staffId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    shopId: { type: mongoose.Schema.Types.ObjectId, ref: "Shop" },
+    amount: { type: Number, required: true },
+    type: { type: String, enum: ["credit", "debit"], required: true },
+    balanceAfterTransaction: { type: Number, default: 0 },
+    date: { type: Date, default: Date.now() }
+}, { timestamps: true })
 
-const walletModel = mongoose.model("Wallet",walletSchema);
-const walletTransactionModel = mongoose.model("WalletTransaction",walletTransactionSchema)
+const walletModel = mongoose.model("Wallet", walletSchema);
+const walletTransactionModel = mongoose.model("WalletTransaction", walletTransactionSchema)
 
-export default {walletModel,walletTransactionModel}; 
+export default { walletModel, walletTransactionModel }; 
