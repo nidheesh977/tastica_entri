9a108a512da54d303c62654598bb8c021b5a319e
Akshai-muraleedharan <akshaimuraleedharan4@gmail.com>
2025-07-17 12:56:15 +0530

    code update in product csv and category csv controller

--- server/controller/csvOrExeclFileUploud/categoryFileUploadController.js
+++ server/controller/csvOrExeclFileUploud/categoryFileUploadController.js
@@ -1,7 +1,6 @@
 import csv from 'csv-parser';
 import fs from 'fs';
 import categoryModel from '../../model/categoryModel.js';
-import shopModel from '../../model/shopModel.js';
 import { generateId } from '../../utils/generateId.js';
 
 
@@ -16,119 +15,113 @@ export const categoryFileUploader = async (req, res) => {
 
         const shopId = req.shop.id;
 
-        const shopName = req.shop.shopName;
-       
+        const countryName = req.shop.countryName;
+
+        const currencyCode = req.shop.currencyCode;
+
         const getCategoryFile = req.file.originalname
 
         const checkCategoryfile = getCategoryFile.includes("categories.csv")
 
-        if(!checkCategoryfile){
-             fs.promises.unlink(filePath, (err) => {
-                                    if (err) console.error("Error deleting file:",);
-                      });
-            return res.status(400).json({ success: false, message: "This file is not categories file"});
+        if (!checkCategoryfile) {
+            fs.promises.unlink(filePath, (err) => {
+                if (err) console.error("Error deleting file:",);
+            });
+            return res.status(400).json({ success: false, message: "This file is not categories file" });
         }
 
-         const categories = [];
-            
+        const categories = [];
+
         fs.createReadStream(filePath)
             .pipe(csv())
             .on('data', (row) => {
-                        
+
 
                 categories.push({
-                    category_id:row.category_id,
+                    category_id: row.category_id,
                     categoryName: row.categoryName.trim().toLowerCase(),
                     description: row.description,
                     discountRate: Number(row.discountRate),
-                    shop:row.shop,
-                    countryName:row.countryName,
-                    currencyCode:row.currencyCode
+                    shop: row.shop,
+                    countryName: row.countryName,
+                    currencyCode: row.currencyCode
                 });
             })
             .on('end', async () => {
                 try {
 
-                    for (const row of categories){
+                    for (const row of categories) {
 
-                        const findShop = await shopModel.findOne({_id:shopId,shopName:row.shop});
-                      
-                        
-                        if(!findShop){
-                          return res.status(400).json({success:false,message:`${shopName} shop is not found`})
-                        }
-  
-                      
 
-                          let categoryId;
-                                              
-                           do {
-                              categoryId = generateId("CATE")
-                               } while (await categoryModel.findOne({category_id:categoryId}));
-                                                     
+
+                        let categoryId;
+
+                        do {
+                            categoryId = generateId("CATE")
+                        } while (await categoryModel.findOne({ category_id: categoryId }));
+
 
                         const isDiscount = row.discountRate > 0 ? true : false;
 
-                        row["shop"] = findShop?._id
-                        row["countryName"] = findShop?.countryName;
-                        row["currencyCode"] = findShop?.currencyCode;
+                        row["shop"] = shopId
+                        row["countryName"] = countryName;
+                        row["currencyCode"] = currencyCode;
                         row["isDiscount"] = isDiscount
                         row["category_id"] = categoryId
 
-                    } 
-                    
-                
+                    }
+
+
 
                     const files = categories.map(row => ({
-                        categoryName:row.categoryName,
-                        shop:row.shop
+                        categoryName: row.categoryName,
+                        shop: row.shop
                     }))
 
-                   
+
                     const existingDocs = await categoryModel.find({
-                        categoryName: {$in:files.map(f => f.categoryName)},
-                        shop: {$in:files.map(f => f.shop)}
+                        categoryName: { $in: files.map(f => f.categoryName) },
+                        shop: { $in: files.map(f => f.shop) }
                     })
 
 
-                   
+
                     const existingSet = new Set(
                         existingDocs.map(doc => `${doc.categoryName}::${doc.shop}`)
                     )
 
 
-                     const newFiles = categories.filter(file => !existingSet.has(`${file.categoryName}::${file.shop}`))
+                    const newFiles = categories.filter(file => !existingSet.has(`${file.categoryName}::${file.shop}`))
 
 
-                       if (newFiles.length === 0) {
+                    if (newFiles.length === 0) {
                         return res.status(400).json({
                             success: false,
                             message: "All categories in the CSV file already exist",
                         });
                     }
 
-                       await categoryModel.insertMany(newFiles);
+                    await categoryModel.insertMany(newFiles);
 
                     res.status(200).json({
                         success: true,
                         message: "CSV data imported successfully",
-                         addedCategories: newFiles.length,
+                        addedCategories: newFiles.length,
                     });
                 } catch (error) {
                     res.status(500).json({ success: false, message: "Internal server error" });
                 } finally {
-                   
+
                     fs.promises.unlink(filePath, (err) => {
                         if (err) console.error("Error deleting file:", err);
                     });
                 }
-            }) 
+            })
             .on('error', (err) => {
-              
+
                 res.status(500).json({ success: false, message: "Error processing file" });
             });
     } catch (error) {
-    
         return res.status(500).json({ success: false, message: "Internal server error" });
     }
 };
